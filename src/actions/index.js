cba363c26764bb9c4d12d7392ae10c898e179fb7
Troy Marchesseault <tmarche@ncsu.edu>
2021-06-08 13:26:54 -0400

    Add UserHeader component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,19 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts')
-
     dispatch({type: "FETCH_POSTS", payload: response.data})
 };
+
+export const fetchUser = (id) => async dispatch => {
+    _fetchUser(id, dispatch);
+}
+
+// The underscore denotes that this is a private function
+// We move the memoized definition outside of the original function so that
+// it doesn't get re-memoized over and over again
+const _fetchUser = _.memoize(async (id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+    dispatch({type: "FETCH_USER", payload: response.data})
+})
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import {combineReducers} from 'redux';
 import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
     // Removes error message from redux
     posts: postsReducer,
+    users: usersReducer,
 });
